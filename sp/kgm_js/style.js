d709293204d1320760bbbad7147c2a2ad71a5b09
kagim <kagimsim@163.com>
2020-05-27 15:32:19 +0800

    kgm changge

--- sp/kgm_js/style.js
+++ sp/kgm_js/style.js
@@ -1,5 +1,4 @@
 // jq渲染开始
-
 $(function(){
     var w_height = $(window).height();
     var w_width = $(window).width();
@@ -7,12 +6,18 @@ $(function(){
     var top;
     var left;
     
-    setInterval(() => {
-        for(var i = 0;i<=len-1;i++){
-            $(".txtwav span").eq(i).css("top",rnd(1,598))
-            $(".txtwav span").eq(i).css("left",rnd(1,598))
-        }
-    }, 2000);
+    setTimeout(() => {
+        $("#hidden-txt").css("top",rnd(25,518))
+        $("#hidden-txt").css("left",rnd(25,518))
+    }, 20);
+
+    // 文字散开
+    // setInterval(() => {
+    //     for(var i = 0;i<=len-1;i++){
+    //         $(".txtwav span").eq(i).css("top",rnd(1,598))
+    //         $(".txtwav span").eq(i).css("left",rnd(1,598))
+    //     }
+    // }, 2000);
 
     function rnd(n, m){
         return Math.floor(Math.random()*(m-n+1)+n)
